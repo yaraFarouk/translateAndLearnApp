71c1a572741b6785e6a02d1a0d00959643e89c4c
al-sakka <abdallahessam501@gmail.com>
2024-08-01 23:56:32 +0300

    adding password visibility

--- lib/views/account_settings_screen.dart
+++ lib/views/account_settings_screen.dart
@@ -14,7 +14,7 @@ class AccountSettingsScreen extends StatefulWidget {
 
 class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
   UserData? userModel;
-
+  bool hiddenPass = true;
   @override
   void initState() {
     super.initState();
@@ -23,8 +23,11 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
 
   void fetchUserData() async {
     String UID = FirebaseAuth.instance.currentUser!.uid;
-    await FirebaseFirestore.instance.collection('users').doc(UID).get().then((onValue)
-    {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(UID)
+        .get()
+        .then((onValue) {
       userModel = UserData.fromFire(onValue.data()!);
     });
 
@@ -34,8 +37,8 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -55,121 +58,131 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
       body: userModel == null
           ? Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              // Avatar
-              Container(
-                margin: EdgeInsets.only(bottom: 25),
-                child: CircleAvatar(
-                  radius: 60,
-                  backgroundImage: AssetImage("assets/images/logo.png"),
-                ),
-              ),
-              // Name
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child: ListTile(
-                  tileColor: Colors.grey[300],
-                  style: ListTileStyle.list,
-                  contentPadding: EdgeInsetsDirectional.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  leading: Icon(Icons.person_outline_rounded),
-                  title: Text(
-                    userModel!.name.toString(),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    // Navigate to change password screen
-                  },
-                ),
-              ),
-              // Email
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child: ListTile(
-                  tileColor: Colors.grey[300],
-                  style: ListTileStyle.list,
-                  contentPadding: EdgeInsetsDirectional.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  leading: Icon(Icons.alternate_email_rounded),
-                  title: Text(
-                    userModel!.email.toString(),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    // Navigate to change password screen
-                  },
-                ),
-              ),
-              // Password
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child: ListTile(
-                  tileColor: Colors.grey[300],
-                  style: ListTileStyle.list,
-                  contentPadding: EdgeInsetsDirectional.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  leading: Icon(Icons.key_rounded),
-                  title: Text(
-                    '******',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    // Navigate to change password screen
-                  },
-                ),
-              ),
-              // Sign Out
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child: ListTile(
-                  tileColor: Colors.red[400],
-                  style: ListTileStyle.list,
-                  contentPadding: EdgeInsetsDirectional.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  leading: Icon(Icons.logout_rounded, color: Colors.white),
-                  title: Text(
-                    "Sign Out",
-                    style: TextStyle(color: Colors.white),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    FirebaseAuth.instance.signOut().then((onValue) async {
-                      print("user logged out successfully");
-                      SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                      await prefs.setBool('hasSeenWelcome', false);
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    // Avatar
+                    Container(
+                      margin: EdgeInsets.only(bottom: 25),
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundImage: AssetImage("assets/images/logo.png"),
+                      ),
+                    ),
+                    // Name
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: ListTile(
+                        tileColor: Colors.grey[300],
+                        style: ListTileStyle.list,
+                        contentPadding: EdgeInsetsDirectional.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        leading: Icon(Icons.person_outline_rounded),
+                        title: Text(
+                          userModel!.name.toString(),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          // Navigate to change password screen
+                        },
+                      ),
+                    ),
+                    // Email
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: ListTile(
+                        tileColor: Colors.grey[300],
+                        style: ListTileStyle.list,
+                        contentPadding: EdgeInsetsDirectional.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        leading: Icon(Icons.alternate_email_rounded),
+                        title: Text(
+                          userModel!.email.toString(),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          // Navigate to change password screen
+                        },
+                      ),
+                    ),
+                    // Password
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: ListTile(
+                        tileColor: Colors.grey[300],
+                        style: ListTileStyle.list,
+                        contentPadding: EdgeInsetsDirectional.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        leading: Icon(Icons.key_outlined),
+                        trailing: IconButton(
+                          onPressed: ()
+                          {
+                            setState(() {
+                              hiddenPass = !hiddenPass;
+                            });
+                          },
+                          icon: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                        title: Text(
+                          hiddenPass ? "******" : userModel!.password.toString(),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          // Navigate to change password screen
+                        },
+                      ),
+                    ),
+                    // Sign Out
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: ListTile(
+                        tileColor: Colors.red[400],
+                        style: ListTileStyle.list,
+                        contentPadding: EdgeInsetsDirectional.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        leading:
+                            Icon(Icons.logout_rounded, color: Colors.white),
+                        title: Text(
+                          "Sign Out",
+                          style: TextStyle(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          FirebaseAuth.instance.signOut().then((onValue) async {
+                            print("user logged out successfully");
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            await prefs.setBool('hasSeenWelcome', false);
 
-                      Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  LanguageSelectionPage()),
-                              (Route<dynamic> route) => false);
-                    });
-                  },
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        LanguageSelectionPage()),
+                                (Route<dynamic> route) => false);
+                          });
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
